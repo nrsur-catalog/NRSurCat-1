f104f124dd610b8f72e19190427707c701e70493
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-08-02 14:24:22 +1200

    update weburl

--- src/nrsur_catalog/__init__.py
+++ src/nrsur_catalog/__init__.py
@@ -7,7 +7,7 @@ from .catalog import Catalog
 from .nrsur_result import NRsurResult
 
 __version__ = "0.0.1"
-__website__ = "https://cjhaster.github.io/NRSurrogateCatalog/"
+__website__ = "https://sxs-collaboration.github.io/"
 
 HERE = os.path.dirname(__file__)
 

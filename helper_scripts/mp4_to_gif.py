5f67773662eb30d9b8196c762c2ff04c7b231bd0
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-09-18 14:12:13 +1200

    add retry

--- helper_scripts/mp4_to_gif.py
+++ helper_scripts/mp4_to_gif.py
@@ -1,4 +1,4 @@
-from tqdm.auto import tqdm
+from tqdm.auto import trange
 import os
 import glob
 
@@ -19,23 +19,29 @@ def get_filesize_in_mb(fn):
 
 def main(mp4_regex, outdir):
     os.makedirs(outdir, exist_ok=True)
-    pbar = tqdm(glob.glob(mp4_regex))
 
-    for mp4_file in pbar:
+    files = glob.glob(mp4_regex)
+    gifs = [os.path.join(outdir, os.path.basename(mp4_file).replace(".mp4", ".gif")) for mp4_file in files]
+    mp4s = [f.replace(".gif", ".mp4") for f in gifs if not os.path.exists(f)]
+
+    pbar = trange(len(mp4s))
+    for i in pbar:
+        mp4_file = mp4s[i]
+        gif_file = gifs[i]
         pbar.set_description(os.path.basename(mp4_file))
-        gif_file = os.path.join(outdir, os.path.basename(mp4_file).replace(".mp4", ".gif"))
         if not os.path.exists(gif_file):
             attempt = 0
             while attempt < 5:
                 try:
                     mp4_to_gif(mp4_file, gif_file)
+                    orig_size = get_filesize_in_mb(mp4_file)
+                    new_size = get_filesize_in_mb(gif_file)
+                    print(f"({orig_size:.2f} MB -> {new_size:.2f} MB)")
                     break
                 except:
+                    if os.path.exists(gif_file):
+                        os.remove(gif_file)
                     attempt += 1
-            orig_size = get_filesize_in_mb(mp4_file)
-            new_size = get_filesize_in_mb(gif_file)
-            print(f"({orig_size:.2f} MB -> {new_size:.2f} MB)")
-
 
 if __name__ == "__main__":
     import argparse

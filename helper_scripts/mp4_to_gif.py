6fe9954e09ea1cf8bbc15a9debca75e879277d97
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-09-18 14:06:19 +1200

    add retry

--- helper_scripts/mp4_to_gif.py
+++ helper_scripts/mp4_to_gif.py
@@ -9,7 +9,8 @@ def mp4_to_gif(mp4_file, gif_file, fps=5):
     clip = VideoFileClip(mp4_file, audio=False)
     clip = clip.resize(0.4)
     clip = clip.speedx(1.5)
-    clip.write_gif(gif_file, fps=fps)
+    clip.write_gif(gif_file, fps=fps, colors=128)
+    return True
 
 
 def get_filesize_in_mb(fn):
@@ -21,11 +22,19 @@ def main(mp4_regex, outdir):
     pbar = tqdm(glob.glob(mp4_regex))
 
     for mp4_file in pbar:
+        pbar.set_description(os.path.basename(mp4_file))
         gif_file = os.path.join(outdir, os.path.basename(mp4_file).replace(".mp4", ".gif"))
-        mp4_to_gif(mp4_file, gif_file)
-        orig_size = get_filesize_in_mb(mp4_file)
-        new_size = get_filesize_in_mb(gif_file)
-        pbar.set_description(f"({orig_size:.2f} MB -> {new_size:.2f} MB)")
+        if not os.path.exists(gif_file):
+            attempt = 0
+            while attempt < 5:
+                try:
+                    mp4_to_gif(mp4_file, gif_file)
+                    break
+                except:
+                    attempt += 1
+            orig_size = get_filesize_in_mb(mp4_file)
+            new_size = get_filesize_in_mb(gif_file)
+            print(f"({orig_size:.2f} MB -> {new_size:.2f} MB)")
 
 
 if __name__ == "__main__":
